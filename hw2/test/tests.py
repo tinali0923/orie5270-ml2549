726c40693cef4ab3bc2fac588d8dba6a25cabcce
tinali0923 <tinali0923@gmail.com>
2018-08-23 18:23:36 -0400

    modified tests

--- hw2/test/tests.py
+++ hw2/test/tests.py
@@ -1,15 +1,18 @@
+from tree.tree import Tree, Node
+
 import unittest
-from tree.tree import Tree,Node
+
 
 class TestPringTree(unittest.TestCase):
-    def test_print_tree(self):
+    def test_printtree(self):
         b = Node(1, None, None)
         c = Node(2, b, None)
         d = Node(5, c, None)
         e = Node(7, None, d)
         a = Tree(e)
 
-        res=[['|', '|', '|', 7, '|', '|', '|'], ['|', '|', '|', '|', 5, '|', '|'], ['|', '|', '|', 2, '|', '|', '|'], ['|', '|', 1, '|', '|', '|', '|']]
+        res = [['|', '|', '|', 7, '|', '|', '|'], ['|', '|', '|', '|', 5, '|', '|'], ['|', '|', '|', 2, '|', '|', '|'],
+               ['|', '|', 1, '|', '|', '|', '|']]
         for i in range(3):
-            res[i]=''.join(list(map(str,res[i])))
-        assert '\n'.join(res)==a.print_tree()
+            res[i] = ''.join(list(map(str, res[i])))
+        assert '\n'.join(res) == a.print_tree()
