0ee1ad2b461478c02351635ea61ed3eb29db6b90
tinali0923 <tinali0923@gmail.com>
2018-08-23 18:24:29 -0400

    Add files via upload

--- hw2/tree/tree.py
+++ hw2/tree/tree.py
@@ -5,7 +5,6 @@ class Tree(object):
     def get_value_root(self):
         """
         This is the function to get the value for the root node.
-
         :return is the value for the root node
         """
         if self.root is not None:
@@ -14,26 +13,28 @@ class Tree(object):
             return None
 
     def print_tree(self):
-        height=self.root.get_height()
-        res=["|"]*height
+        height = self.root.get_height()
+        res = ["|"] * height
         for i in range(height):
-            res[i]=["|"]*(2*height-1)
-        a=0
-        b=height-1
-        node=self.root
-        def recur(res,node,a,b):
+            res[i] = ["|"] * (2 * height - 1)
+        a = 0
+        b = height - 1
+        node = self.root
+
+        def recur(res, node, a, b):
             if node is not None:
-                res[a][b]=node.value
+                res[a][b] = node.value
                 if node.right is not None:
-                    res=recur(res,node.right,a+1,b+1)
+                    res = recur(res, node.right, a + 1, b + 1)
                 if node.left is not None:
-                    res=recur(res,node.left,a+1,b-1)
+                    res = recur(res, node.left, a + 1, b - 1)
                 return res
             else:
                 return res
-        res=recur(res,node,a,b)
+
+        res = recur(res, node, a, b)
         for i in range(height):
-            res[i]=''.join(list(map(str,res[i])))
+            res[i] = ''.join(list(map(str, res[i])))
         return '\n'.join(res)
 
 
@@ -43,31 +44,28 @@ class Node(object):
         self.right = right
         self.left = left
 
-
     def get_height(self):
         if self.value is None:
             return 0
         else:
             if self.left != None:
-                left=self.left.get_height()
+                left = self.left.get_height()
             else:
-                left=0
+                left = 0
 
-            if self.right !=None:
-                right=self.right.get_height()
+            if self.right != None:
+                right = self.right.get_height()
             else:
-                right=0
+                right = 0
 
-            return 1+max(left,right)
+            return 1 + max(left, right)
 
 
 if __name__ == "__main__":
-    b=Node(1,None,None)
-    c=Node(2,b,None)
-    d=Node(5,c,None)
-    e=Node(7,None,d)
-    a=Tree(e)
+    b = Node(1, None, None)
+    c = Node(2, b, None)
+    d = Node(5, c, None)
+    e = Node(7, None, d)
+    a = Tree(e)
 
     print a.print_tree()
-
-
